180fd43ea34ac86d229f4547f54bf146e9a25c28
Rubesh Chandar <rubeshchander.rc@gmail.com>
2025-02-22 02:20:49 +0000

    some new models have been added

--- split/admin.py
+++ split/admin.py
@@ -4,7 +4,8 @@ from .models import *
 
 
 class GroupAdmin(admin.ModelAdmin):
-    readonly_fields = ("created_at", "modified", "created_by",)
+    readonly_fields = ("created_at", "modified", "created_by", )
+    prepopulated_fields = {"slug": ("name",)}
 
     def save_model(self, request, obj, form, change):
         if not obj.pk:  # Only set created_by when creating a new Group
@@ -12,4 +13,15 @@ class GroupAdmin(admin.ModelAdmin):
         super().save_model(request, obj, form, change)
 
 
+class GroupBalanceAdmin(admin.ModelAdmin):
+    list_display = ("get_username", "user", "group", "balance")
+    readonly_fields = ("modified",)
+
+    def get_username(self, obj):
+        return str(obj.user.username).capitalize()
+
+    get_username.short_description = "Username"
+
+
 admin.site.register(Group, GroupAdmin)
+admin.site.register(GroupBalance, GroupBalanceAdmin)
--- split/models.py
+++ split/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from users.models import CustomUser
 
-# Create your models here.
-
 
 class Group(models.Model):
     name = models.CharField(max_length=50)
@@ -11,6 +9,34 @@ class Group(models.Model):
         CustomUser, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(default="", db_index=True, null=False)
 
     def __str__(self):
         return self.name
+
+
+class GroupBalance(models.Model):
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="groupbalances")
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username} -> {self.group} : {self.balance}"
+
+    class Meta:
+        unique_together = ('user', 'group')
+        verbose_name = ("Group Balance")
+
+
+class Expense(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    paid_by = models.ForeignKey(
+        CustomUser, on_delete=models.DO_NOTHING, related_name="paid_expenses")
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(
+        CustomUser, on_delete=models.DO_NOTHING, related_name="created_expenses")
