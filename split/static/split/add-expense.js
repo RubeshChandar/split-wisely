2080f841b353a447d470b7dcd32e3bcca4fcd528
Rubesh Chandar <rubeshchander.rc@gmail.com>
2025-03-05 02:07:19 +0000

    some minor ui updates

--- split/static/split/add-expense.js
+++ split/static/split/add-expense.js
@@ -9,6 +9,7 @@
         if (!checkbox.checked) {
           amountInput.value = ""
         }
+        distributeAmountEqually()
       }
 
       updateAmountInputState(); // Initial state
@@ -18,26 +19,32 @@
   });
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    const splitEquallySpan = document.getElementById('split-equally');
+function distributeAmountEqually() {
     const amountInput = document.getElementById('id_amount');
     const checkboxes = document.querySelectorAll('input[class="form-check-input mt-0"]');
     const amountInputs = document.querySelectorAll('input[name="split_amount"]');
 
-    splitEquallySpan.addEventListener('click', function() {
-        const totalAmount = parseFloat(amountInput.value) || 0;
-        const checkedCheckboxes = Array.from(checkboxes).filter(checkbox => checkbox.checked);
-        const checkedCount = checkedCheckboxes.length;
-        console.log(checkedCheckboxes)
+    const totalAmount = parseFloat(amountInput.value) || 0;
+    const checkedCheckboxes = Array.from(checkboxes).filter(checkbox => checkbox.checked);
+    const checkedCount = checkedCheckboxes.length;
+    console.log(checkedCheckboxes);
 
-        if (checkedCount > 0) {
-            const share = totalAmount / checkedCount;
-            amountInputs.forEach(amountInput => {
-                const checkbox = amountInput.closest('.input-group').querySelector('input[type="checkbox"]');
-                if (checkbox.checked) {
-                    amountInput.value = share.toFixed(2);
-                }
-            });
-        }
-    });
+    if (checkedCount > 0) {
+        const share = totalAmount / checkedCount;
+        amountInputs.forEach(amountInput => {
+            const checkbox = amountInput.closest('.input-group').querySelector('input[type="checkbox"]');
+            if (checkbox.checked) {
+                amountInput.value = share.toFixed(2);
+            }
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const splitEquallySpan = document.getElementById('split-equally');
+    const amountInput = document.getElementById('id_amount');
+
+    amountInput.addEventListener('keyup',distributeAmountEqually)
+    splitEquallySpan.addEventListener('click', distributeAmountEqually);
 });
+
